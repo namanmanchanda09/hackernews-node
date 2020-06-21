be6ee084b1f6dce8dc7182eaad5083388d95aa99
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-22 00:45:16 +0530

    added the DataBase persistence using Prisma Client

--- src/index.js
+++ src/index.js
@@ -1,36 +1,33 @@
 const {GraphQLServer} = require('graphql-yoga');
+const {prisma} = require('./generated/prisma-client')
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length;
 const resolvers = {
     Query :{
         info: () => `This is the API of Hackernews clone.`,
-        feed: () => links,
+        feed: (root,args,context,info) => {
+            return context.prisma.links()
+        },
     },
     Mutation: {
-        post: (parent,args) => {
-            const link = {
-                id: `link-${idCount++}`,
+        post : (root,args, context) => {
+            return context.prisma.createLink({
+                url: args.url,
                 description: args.description,
-                url: args.url
-            }
-            links.push(link);
-            return link;
-        }
-    }
+            })
+        },
+    },
 }
+
 
 const server = new GraphQLServer({
     typeDefs : './src/schema.graphql',
-    resolvers
+    resolvers,
+    context:{prisma}
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
+
+
 
 
 
